fa40e74cb02b28d69d74665dfc18b582c97acc3c
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-03-20 15:37:15 +0530

    update

---
+++ swiper.js
@@ -0,0 +1,9 @@
+let swiper=new Swiper(".swiper-slide",{
+    navigation:{
+        nextE1:".swiper-button-next",
+        prevE1:".swiper-button-prev"
+    },
+    autoplay:{
+        delay:2000,
+    }
+})
